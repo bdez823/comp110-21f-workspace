5a14cf35d36cc9345fc2a7e603f91281ff50782c
Bruce Fernandeez <bdez823@email.unc.edu>
2021-09-05 13:02:18 -0400

    updated finished repeat_beat ex02 exercise

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -7,15 +7,12 @@ user_str_input: str = input("What beat do you want to repeat? ")
 user_int_input: int = int(input("How many times do you want to repeat it? "))
 user_str_gap: str = (" " + user_str_input)
 user_stored_input: str = (user_str_input)
+user_int_minus: int = int(user_int_input - 1)
 i: int = 0
-j: int = 1
 if user_int_input <= 0:
     print("No beat...")
-else:
-    while i < user_int_input:
-        if i <= j:
-            user_stored_input = user_stored_input + user_str_gap
-        i = i + 1
-
-if i == user_int_input:
+while i < user_int_minus:
+    user_stored_input = user_stored_input + user_str_gap
+    i = i + 1
+if i == user_int_minus:
     print(user_stored_input)
