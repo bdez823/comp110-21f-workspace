f811d4d04bc251fe51ccd53d09ca05ec5cc857d1
Bruce Fernandeez <bdez823@email.unc.edu>
2021-09-12 19:22:25 -0400

    finished ex03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,26 @@
 """Finding duplicate letters in a word."""
 
 __author__ = "123456789"
+
+user_word: str = input("Enter a word: ")
+i: int = 0
+j: int = 1
+t: int = 1
+bool_storage: bool = False
+number_of_characters: int = (len(user_word))
+number_of_characters_minus_two: int = (len(user_word) - 2)
+
+while i < number_of_characters:
+    # isolates letter from user_word input
+    first_letter_identifier: str = user_word[i]
+    # Loop to compare specific letter to others in string
+    while j < number_of_characters:
+        second_letter_identifier: str = user_word[j]
+        if first_letter_identifier == second_letter_identifier:
+            bool_storage = True    
+        j = j + 1
+    # lines 23 and 24, reseting counter and adding one, in order to run sub loop and to make sure loop not evaluating same value twice.
+    t = t + 1
+    j = t   
+    i = i + 1
+print("Found duplicate: " + str(bool_storage))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -4,3 +4,16 @@ __author__ = "123456789"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+tree_string: str = TREE
+depth: int = int(input("Depth: "))
+i: int = 0
+depth_plus_one: int = (depth + 1)
+
+if depth >= 1:
+    while i < depth_plus_one:    
+        if i == 0:
+            print(TREE)
+        if i > 1:
+            tree_string = tree_string + TREE
+            print(tree_string)
+        i = i + 1
